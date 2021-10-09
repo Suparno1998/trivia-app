fb00dc1b4e01930b5e01f225853a0de1ae9b6ea2
Suparno1998 <ssuparno1998@gmail.com>
2021-10-09 12:40:04 +0530

    Remove jsx files

--- src/components/answer.js
+++
@@ -1,9 +0,0 @@
-export default function Answer(props) {
-    function handleChange(event){
-        let val = event.target.value
-        props.answer(val);
-    }
-    return (
-            <input type="text" className="form-control" placeholder="Your Answer.... " required onChange={handleChange} value={props.val}></input>
-        );
-}
--- src/components/question.js
+++
@@ -1,7 +0,0 @@
-export default function Question(props) {
-    return (
-        <div>
-            <p className="small">Category : {props.category}</p>
-            <h2>{props.question}</h2>
-        </div>);
-}
